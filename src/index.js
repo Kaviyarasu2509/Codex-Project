d26bf1ab2dd18fbc41d6b68cfc39670424e2b5a6
Kaviyarasu <kaviyarasu01@gmail.com>
2025-11-12 06:15:58 +0530

    delete the same file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";  // Import BrowserRouter
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -15,4 +15,4 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
+
